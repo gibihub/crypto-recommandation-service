cf51e0da6f074a4c218f171d47b34917b93a5621
Raul Giblak <raul.giblak98@e-uvt.ro>
2024-11-07 16:33:00 +0200

    Add spring initializr

---
+++ src/test/java/org/crypto/recommendations/crypto_recommendation_service/CryptoRecommendationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.crypto.recommendations.crypto_recommendation_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoRecommendationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
